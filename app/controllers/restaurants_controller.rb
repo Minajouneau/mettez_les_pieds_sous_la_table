9bbef4801344e0a53a67ce7500189e93bf867fda
stanislas legrez <stanislas27@gmail.com>
2020-08-24 18:22:05 +0200

    In progress

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,19 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.geocoded # returns flats with coordinates
 
-private
-
-  def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :contact_email, :activated, :domain_name, :description, :phone_number  photos: [])
+    @markers = @restaurants.map do |restaurant|
+      {
+        lat: restaurant.latitude,
+        lng: restaurant.longitude
+      }
+    end
   end
+
+# private
+
+  # def restaurant_params
+  #   params.require(:restaurant).permit(:name, :address, :contact_email, :activated, :domain_name, :description, :phone_number  photos: [])
+  # end
 
 end
