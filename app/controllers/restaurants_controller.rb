b3596f5acca19636d7fb2517047a646439aa6c4a
mina jouneau <jouneaumina@gmail.com>
2020-08-25 13:09:30 +0200

    add card in index

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,9 +1,33 @@
 class RestaurantsController < ApplicationController
 
-private
-
-  def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :contact_email, :activated, :domain_name, :description, :phone_number  photos: [])
+  def index
+    @user = current_user
   end
+
+  # def new
+  #   @restaurant = Restaurant.new
+  # end
+
+  # def create
+  #   @user = current_user
+  #   @restaurant = Restaurant.new(restaurant_params)
+  #   @restaurant.user = @user
+  #   if @restaurant.save
+  #     redirect_to restaurant_path(@restaurant)
+  #   else
+  #     render "new"
+  #   end
+  # end
+
+  # def edit
+  #   @restaurant = Restaurant.find(params[:id])
+  #   authorize @restaurant
+  # end
+
+# private
+
+#   def restaurant_params
+#     params.require(:restaurant).permit(:name, :address, :contact_email, :activated, :domain_name, :description, :phone_number  photos: [])
+#   end
 
 end
