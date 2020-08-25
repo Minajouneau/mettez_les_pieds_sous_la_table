42709e9ea3dcfdcc05e36d82119c6198eba83896
stanislas legrez <stanislas27@gmail.com>
2020-08-25 11:22:45 +0200

    Added map on edit, began front show

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -9,29 +9,21 @@ const initMapbox = () => {
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v10'
     });
-  }
-};
-
-export { initMapbox };
-
-if (mapElement) {
-    // [ ... ]
     const markers = JSON.parse(mapElement.dataset.markers);
     markers.forEach((marker) => {
       new mapboxgl.Marker()
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(map);
     });
+    fitMapToMarkers(map, markers);
   }
+};
+
+export { initMapbox };
 
 const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
     map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
 };
-
-if (mapElement) {
-    // [ ... ]
-    fitMapToMarkers(map, markers);
-}
   
