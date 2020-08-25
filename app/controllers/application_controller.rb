a98282e295a76cf0f4d7df61dd596534b565ed63
Francois Nguyen <fnguyen90@gmail.com>
2020-08-25 11:28:27 +0200

    seed user + restaurant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,15 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  private
-
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(root_path)
   end
   
+  private
+  
   def skip_pundit?
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
+
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,10 +5,23 @@ class RestaurantsController < ApplicationController
     @restaurants = policy_scope(Restaurant)
   end
 
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant 
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    authorize @restaurant
+    @restaurant.update(restaurant_params)
+    redirect_to dashboard_path
+    # Will raise ActiveModel::ForbiddenAttributesError
+  end
+
 private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :contact_email, :activated, :domain_name, :description, :phone_number  photos: [])
+    params.require(:restaurant).permit(:name, :address, :contact_email, :activated, :domain_name, :description, :phone_number, photos: [])
   end
 
 end
