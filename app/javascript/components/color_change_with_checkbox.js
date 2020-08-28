703a5d2d0f80be95cdbc5e86211a90da09f258f9
stanislas legrez <stanislas27@gmail.com>
2020-08-28 14:41:54 +0200

    Added changes

--- app/javascript/components/color_change_with_checkbox.js
+++ app/javascript/components/color_change_with_checkbox.js
@@ -1,13 +1,15 @@
 const ColorChangeWithCheckbox = () => {
   const checkbox = document.getElementById('restaurant_activated');
   // const messageToColor = document.getElementById('');
-  checkbox.addEventListener( 'change', (event) => {
-      if(event.currentTarget.checked) {
-          console.log(event.currentTarget.checked)
-      } else {
-          console.log(event.currentTarget.checked)
-      }
-  });
+  if (checkbox) {
+      checkbox.addEventListener( 'change', (event) => {
+          if(event.currentTarget.checked) {
+              console.log(event.currentTarget.checked)
+          } else {
+              console.log(event.currentTarget.checked)
+          }
+      });
+  }
 };
 
 export { ColorChangeWithCheckbox };
