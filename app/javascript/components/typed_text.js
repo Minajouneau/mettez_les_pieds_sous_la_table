590de8712070c023c88a25d11360c3318b847d58
stanislas legrez <stanislas27@gmail.com>
2020-08-29 17:22:24 +0200

    Updated some content

--- app/javascript/components/typed_text.js
+++ app/javascript/components/typed_text.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#typed-text', {
-    strings: ["« Mon engagement envers la pomme de terre »", "Vive les lardons"],
+    strings: ["« Mon engagement envers la pomme de terre »", "« Vive les lardons »"],
     typeSpeed: 50,
     loop: true
   });
