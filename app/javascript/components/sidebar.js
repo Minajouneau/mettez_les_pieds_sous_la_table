851fe315f5db7d7405bfcc153e122226687d5ae0
mina jouneau <jouneaumina@gmail.com>
2020-09-02 12:31:32 +0200

    changes

---
+++ app/javascript/components/sidebar.js
@@ -0,0 +1,41 @@
+
+
+// const slideSidenav = () => {
+// const side = document.querySelector('.sidenav-element');
+// const burger = document.querySelector('.burger');
+// console.log(burger)
+// burger.addEventListener("click", (event)=>{
+// side.classList.toggle("sidenav-hidden")
+// side.classList.toggle("sidebar-container")
+// });
+
+
+
+// }
+
+
+// export {slideSidenav};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var elem = document..getElementsByClassName('sidenav')
+// element.addEventListener("click", slide);
+
+// function openSide() {
+//     document.getElementByClassName("sidenav").style.width = "100%";
+// }
+
+// function closeSide() {
+//     document.getElementByClassName("sidenav").style.width = "100%";
+// }
